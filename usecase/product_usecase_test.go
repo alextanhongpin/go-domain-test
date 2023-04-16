28703dbe79316d71605faee99bbbcc3844d25f31
alextanhongpin <alextan220990@gmail.com>
2023-04-16 10:32:29 +0800

    chore: update tests

--- usecase/product_usecase_test.go
+++ usecase/product_usecase_test.go
@@ -2,10 +2,11 @@ package usecase_test
 
 import (
 	"context"
+	"errors"
 	"testing"
 	"time"
 
-	"github.com/alextanhongpin/go-core-microservice/types/errors"
+	"github.com/alextanhongpin/go-domain-test/domain"
 	"github.com/alextanhongpin/go-domain-test/domain/factories"
 	"github.com/alextanhongpin/go-domain-test/mocks"
 	"github.com/alextanhongpin/go-domain-test/types"
@@ -16,50 +17,83 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
-var isContext = mock.MatchedBy(func(ctx context.Context) bool {
-	return true
-})
+var (
+	isContext = mock.MatchedBy(func(ctx context.Context) bool {
+		return true
+	})
+)
 
 func TestProductUsecase(t *testing.T) {
-	t.Run("success", func(t *testing.T) {
-		assert := assert.New(t)
+	type stub struct {
+		findByID    *domain.Product
+		findByIDErr error
+	}
 
-		pdt := factories.NewProduct()
-		productRepo := new(mocks.ProductRepository)
-		productRepo.On("FindByID", isContext, pdt.ID).Return(pdt, nil).Once()
+	type args struct {
+		id uuid.UUID
+	}
 
-		uc := usecase.NewProduct(productRepo)
-		ucPdt, err := uc.View(context.Background(), pdt.ID)
-		assert.Nil(err)
-		if diff := cmp.Diff(pdt, ucPdt); diff != "" {
-			t.Errorf("want (+), got (-): %v", diff)
-		}
-	})
+	p := factories.NewProduct()
 
-	t.Run("not published", func(t *testing.T) {
-		assert := assert.New(t)
+	tests := []struct {
+		name    string
+		args    args
+		stub    stub
+		want    *domain.Product
+		wantErr error
+	}{
+		{
+			name: "success",
+			args: args{id: p.ID},
+			stub: stub{findByID: p},
+			want: p,
+		},
+		{
+			name: "not yet published",
+			args: args{id: p.ID},
+			stub: stub{
+				findByID: func() *domain.Product {
+					cp := *p
+					cp.PublishedAt = nil
+					return &cp
+				}(),
+			},
+			wantErr: usecase.ErrProductNotFound,
+		},
+		{
+			name: "published in the future",
+			args: args{id: p.ID},
+			stub: stub{
+				findByID: func() *domain.Product {
+					cp := *p
+					cp.PublishedAt = types.Ptr(time.Now().Add(1 * time.Second))
+					return &cp
+				}(),
+			},
+			wantErr: usecase.ErrProductNotFound,
+		},
+		{
+			name:    "failed",
+			args:    args{id: p.ID},
+			stub:    stub{findByIDErr: errors.New("db error")},
+			wantErr: errors.New("db error"),
+		},
+	}
 
-		pdt := factories.NewProduct()
-		pdt.PublishedAt = types.Ptr(time.Now().Add(1 * time.Second)) // Only published 1s in the future.
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			assert := assert.New(t)
 
-		productRepo := new(mocks.ProductRepository)
-		productRepo.On("FindByID", isContext, pdt.ID).Return(pdt, nil).Once()
+			productRepo := new(mocks.ProductRepository)
+			productRepo.On("FindByID", isContext, tc.args.id).Return(tc.stub.findByID, tc.stub.findByIDErr).Once()
 
-		uc := usecase.NewProduct(productRepo)
-		ucPdt, err := uc.View(context.Background(), pdt.ID)
-		assert.Equal(usecase.ErrProductNotFound, err)
-		assert.Nil(ucPdt)
-	})
-
-	t.Run("failed", func(t *testing.T) {
-		assert := assert.New(t)
-
-		productRepo := new(mocks.ProductRepository)
-		productRepo.On("FindByID", isContext, uuid.Nil).Return(nil, errors.New("db error")).Once()
-
-		uc := usecase.NewProduct(productRepo)
-		ucPdt, err := uc.View(context.Background(), uuid.Nil)
-		assert.Equal(errors.New("db error"), err)
-		assert.Nil(ucPdt)
-	})
+			uc := usecase.NewProduct(productRepo)
+			ucPdt, err := uc.View(context.Background(), tc.args.id)
+			assert.Equal(tc.wantErr, err)
+			if diff := cmp.Diff(tc.want, ucPdt); diff != "" {
+				t.Errorf("want (+), got (-): %v", diff)
+			}
+		})
+	}
 }
