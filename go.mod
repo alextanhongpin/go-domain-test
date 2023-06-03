8eff37e39df88eafeb143b748268fa9baa920d58
alextanhongpin <alextan220990@gmail.com>
2023-06-03 16:11:53 +0800

    chore: update example

--- go.mod
+++ go.mod
@@ -3,17 +3,17 @@ module github.com/alextanhongpin/go-domain-test
 go 1.20
 
 require (
-	github.com/BurntSushi/toml v1.2.1
-	github.com/alextanhongpin/go-core-microservice v0.0.0-20230415071410-b2a7378ff01b
-	github.com/google/go-cmp v0.5.8
+	github.com/alextanhongpin/errcodes v0.0.0-20230602163126-0444dce7a961
 	github.com/google/uuid v1.3.0
-	github.com/stretchr/testify v1.8.2
+	github.com/stretchr/testify v1.8.4
 )
 
 require (
-	github.com/alextanhongpin/errors v0.14.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/objx v0.5.0 // indirect
+	google.golang.org/grpc v1.54.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- usecase/errors.go
+++ usecase/errors.go
@@ -1,19 +1,12 @@
 package usecase
 
 import (
-	_ "embed"
-
-	"github.com/BurntSushi/toml"
-	"github.com/alextanhongpin/go-core-microservice/types/errors"
+	"github.com/alextanhongpin/errcodes"
 )
 
+// Product errors.
 var (
-	//go:embed errors.toml
-	errorBytes []byte
-	_          = errors.MustLoad(errorBytes, toml.Unmarshal)
-
-	// Product errors.
-	ErrProductNotFound      = errors.Get("product.not_found")
-	ErrProductUnauthorized  = errors.Get("product.unauthorized")
-	ErrProductNameBadFormat = errors.Get("product.name_bad_format")
+	ErrProductNotFound      = errcodes.New(errcodes.NotFound, "product_not_found", "Product does not exist or may have been deleted.")
+	ErrProductUnauthorized  = errcodes.New(errcodes.Unauthorized, "product_unauthorized", "You do not have access to this product")
+	ErrProductNameBadFormat = errcodes.New(errcodes.BadRequest, "product_name_bad_format", "Product name can only contain alphanumeric characters and spaces.")
 )
