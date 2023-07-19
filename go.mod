7c8f5fd84b616536f6ec43ea82ba3b531e6c7c45
alextanhongpin <alextan220990@gmail.com>
2023-07-19 23:51:16 +0800

    chore: update errors

--- go.mod
+++ go.mod
@@ -3,21 +3,17 @@ module github.com/alextanhongpin/go-domain-test
 go 1.20
 
 require (
-	github.com/alextanhongpin/errcodes v0.0.0-20230602163126-0444dce7a961
+	github.com/alextanhongpin/errors v0.0.0-20230717124106-3e3c39edaa89
 	github.com/google/uuid v1.3.0
 	github.com/stretchr/testify v1.8.4
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/objx v0.5.0 // indirect
-	github.com/vektra/mockery v1.1.2 // indirect
-	golang.org/x/mod v0.2.0 // indirect
-	golang.org/x/tools v0.0.0-20200323144430-8dcfad9e016e // indirect
-	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543 // indirect
-	google.golang.org/grpc v1.54.0 // indirect
-	google.golang.org/protobuf v1.28.1 // indirect
+	google.golang.org/grpc v1.56.2 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- usecase/errors.go
+++ usecase/errors.go
@@ -1,12 +1,13 @@
 package usecase
 
 import (
-	"github.com/alextanhongpin/errcodes"
+	"github.com/alextanhongpin/errors/causes"
+	"github.com/alextanhongpin/errors/codes"
 )
 
 // Product errors.
 var (
-	ErrProductNotFound      = errcodes.New(errcodes.NotFound, "product_not_found", "Product does not exist or may have been deleted.")
-	ErrProductUnauthorized  = errcodes.New(errcodes.Unauthorized, "product_unauthorized", "You do not have access to this product")
-	ErrProductNameBadFormat = errcodes.New(errcodes.BadRequest, "product_name_bad_format", "Product name can only contain alphanumeric characters and spaces.")
+	ErrProductNotFound      = causes.New(codes.NotFound, "product_not_found", "Product does not exist or may have been deleted.")
+	ErrProductUnauthorized  = causes.New(codes.Unauthorized, "product_unauthorized", "You do not have access to this product")
+	ErrProductNameBadFormat = causes.New(codes.BadRequest, "product_name_bad_format", "Product name can only contain alphanumeric characters and spaces.")
 )
